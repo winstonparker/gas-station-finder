7e38f2e8538b17dd7b4eb243a2a8712985e2c9ce
Winston <winston.parker@24g.com>
2020-03-21 19:40:16 -0400

    Added copy changes for initial action

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
-// Your code goes here
 
-// ----- PLEASE NOTE -----
-// Although your code is bundled, we do not otherwise transform or transpile your
-// user code. ES6+ JavaScript is incompatible with the in-vehicle platform and
-// will need to be transpiled into ES5!
-// -----------------------
 
-// You can require definitions from other JavaScript and JSON files.
-// Your code will be bundled together using WebPack during DFF compilation.
+
 var terms = require("./terms.json");
 // Load each term into built-in flow context for Terms & Conditions.
 ngi.cards("_ngi:terms", terms);
@@ -18,18 +11,22 @@ ngi.cards('_ngi:init.splash', {
 });
 
 ngi.flow('myFlow', {
-    entry: 'myView' // The first view the task will load.
+    entry: 'welcomeScreen' // The first view the task will load.
   })
-  .addRoute('myView', {
+  .addRoute('welcomeScreen', {
     layout: 'Detail',
-
+    action: function() {
+      // We include Lodash with the DFF as well!
+      var bodyText = _.get(this, 'body');
+      ngi.vehicle.speak(bodyText);
+    },
     // Views can have actions too!
     // Press the button and take a look in your developer console.
     // The DFF provides context-based navigation and rendering information,
     //   all on the 'this' property of your functions.
 
     actions: [{
-      label: 'Hello!',
+      label: 'Locate Nearby Stations',
       action: function() {
         console.log('HI!!!!', this);
         // We include Lodash with the DFF as well!
@@ -39,17 +36,14 @@ ngi.flow('myFlow', {
     }]
   });
 
-ngi.cards('myFlow.myView', {
+ngi.cards('myFlow.welcomeScreen', {
   title: 'Gas Station Locator',
-  // Many layouts are responsive to the content they are rendering.
-  // Try uncommenting the next line and see how the layout responds.
   // images: ['./images/otterlyadorable.jpg'],
 
-  // Body text may contain _simple_ HTML for formatting purposes.
   // The following tags are allowed: <b>, <i>, <p>, <br>, <u>, <em>, <strong>, <pre>, <span>.
   // The following classes are allowed: text-left, text-right, text-center, light, medium, bold.
-  // Anything outside the whitelist will be stripped from the string as it is displayed.
-  body: '<p>Welcome to the Driver First Framework</p> <br><p class="text-left">Hurray!</p>'
+  body: '<p>Welcome to the Gas Station Locator. This application finds the 10 closest gas stations to your current position.</p>',
+
 });
 
 // Specify your own entry flow, this will connect to Splash, Terms, and About.
